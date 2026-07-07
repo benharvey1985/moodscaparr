a98d2cfae5b55562652358775476618e893cdd35
Moodscaparr Dev <dev@moodscaparr.app>
2026-07-07 20:47:49 +0100

    fix: remove explicit children prop from DropdownMenuTrigger
    The implicit children from MenuPrimitive.Trigger.Props is sufficient.

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -14,10 +14,8 @@ function DropdownMenuPortal({ ...props }: MenuPrimitive.Portal.Props) {
   return <MenuPrimitive.Portal data-slot="dropdown-menu-portal" {...props} />
 }
 
-function DropdownMenuTrigger({ children, ...props }: MenuPrimitive.Trigger.Props) {
-  return <MenuPrimitive.Trigger data-slot="dropdown-menu-trigger" {...props}>
-    {children}
-  </MenuPrimitive.Trigger>
+function DropdownMenuTrigger({ ...props }: MenuPrimitive.Trigger.Props) {
+  return <MenuPrimitive.Trigger data-slot="dropdown-menu-trigger" {...props} />
 }
 
 function DropdownMenuContent({
